0ed3ae96b6f2c06923c79e99fd3c0ad2cc00b5bc
Pete VS <pete_vs88@me.com>
2021-12-14 12:18:50 -0700

    made some basic updates

--- src/containers/Greetings/Greetings.jsx
+++ src/containers/Greetings/Greetings.jsx
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import icon from '../../assets/img/icon-128.png';
 
-class GreetingComponent extends Component {
-  state = {
-    name: 'dev',
-  };
+const Greetings = () => {
 
-  render() {
-    return (
-      <div>
-        <p>Hello, {this.state.name}!</p>
-        <img src={icon} alt="extension icon" />
-      </div>
-    );
+  const [currentPrice, setCurrentPrice] = useState(0)
+
+  const baseURL = 'https://api.coingecko.com/api/v3/coins/bitcoin'
+
+  const getCurrentPrice = async (currency) => {
+    const response = await fetch(baseURL)
+    const { market_data } = await response.json()
+    const { current_price } = market_data
+    return current_price[currency]
+    }
+
+  const handleClick = async () => {
+    const price = await getCurrentPrice('cad')
+    setCurrentPrice(price)
   }
+
+  useEffect(() => {
+    const setPrice = async () => {
+      const price = await getCurrentPrice('cad')
+      setCurrentPrice(price)
+    }
+
+    setPrice()
+  }, [])
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Hello
+      </button>
+      <p>Bitcoin Price: {currentPrice}</p>
+    </>
+  )
+
 }
 
-export default GreetingComponent;
+export default Greetings
